c0c2df6af6940eb63712daea8372c8b89f662b45
ภัทรภรณ์ รัตนบุตร <lookmaiipr27@gmail.com>
2016-10-08 15:31:14 +0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,4 @@
+5.
+US_states.keep_if {|v| v =~ /[MNO]/} 
+6.
+  puts US_states.reverse!
---
+++ oop.rb
@@ -0,0 +1,8 @@
+pi = 3.14159;
+r = 5;
+area = pi*r*r ;
+circumference = 2*pi*r ;
+
+ puts "Area is #{area}"
+ puts "Diameter is #{r}"
+ puts "Circumference is #{circumference}"
